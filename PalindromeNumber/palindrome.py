daec9dd5d3eb8092806973f0fc4e1d7a1b6dda17
Aniket Singh <singh.aniket1406@gmail.com>
2024-12-09 16:09:32 +0530

    Modified the same code using string approach
    Runtime: 0ms (100%)
    Memory: 17.42 MB( 7.16%)

--- PalindromeNumber/palindrome.py
+++ PalindromeNumber/palindrome.py
@@ -1,13 +1,5 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        
-        if x < 0 or (x % 10 == 0 and x != 0):
-            return False
+        str_x = str(x)
 
-        check_x = x
-        reversed_x = 0
-        while x > 0:
-            reversed_x = reversed_x * 10 + (x % 10)
-            x //=10
-
-        return check_x == reversed_x
+        return str_x == str_x[::-1]
